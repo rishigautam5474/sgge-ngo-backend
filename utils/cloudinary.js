4c1e5dd4b2812dd7433844c21e4a2de51ec98f5b
Rishi RishiGautam5474 <rishigautam5474@gmail.com>
2025-07-18 15:43:51 +0530

    cloudinary changes

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,7 +1,6 @@
 import env from "dotenv";
 env.config();
 import { v2 as cloudinary } from "cloudinary";
-import fs from "fs";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
