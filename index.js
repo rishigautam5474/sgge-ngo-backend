dd1ab549685ecc31478695e82650ec844b0b236d
Rishi RishiGautam5474 <rishigautam5474@gmail.com>
2025-07-18 22:47:26 +0530

    change cors origin

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ MongoDbConnection(process.env.MONGOOSE_URI)
   });
 
 app.use(cors({
-  origin: "http://localhost:5173"
+  origin: "https://sggengo-live.onrender.com"
 }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
